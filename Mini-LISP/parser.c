f0863a423066c8a83cfd3ab6ee59aeccfa02a4b1
Yossi Gil <yogi@cs.technion.ac.il>
2021-02-13 08:33:01 +0200

    sync

--- Mini-LISP/parser.c
+++ Mini-LISP/parser.c
@@ -1,4 +1,4 @@
-#include "tokenizer.h"
+#include "parser.h"
 #include "dump.h"
 
 #define SILENT 1 
@@ -7,82 +7,16 @@
 #define D(...) 0
 #endif
 
-namespace Tokenizer {
-static char *head;
+namespace Parser {
 
-static inline char& C() {
-  return *head;
-}
-extern void initialize(char *buffer) {
-  head = buffer;
+extern enum Status status() {
+  return ready;
 }
 
-static char advance();
-static H nextAtom();
-static bool isToken();
-static bool isAtom();
-static bool isNewLine(); 
-static bool isIgnored();
-
-extern H next() {
-  D(C(), head, isToken(), isIgnored(), isAtom(), isNewLine());
-  if (!advance())
-    return $;
-  if (isToken())
-    return *head++;
-  D(C(), head, isToken(), isIgnored(), isAtom(), isNewLine());
-  return nextAtom();
+extern void go(char *buffer) {
+  Tokenizer::init(buffer);
 }
 
-static char advance() {
-  D(head,C(), (long)head, (int)C());
-  for (; ; ++head) {
-    D(head,C(), (long)head, (int)C());
-    if (C() == '\0')
-      break;
-    if (isIgnored())
-      continue;
-    if (C() != ';')
-      break;
-    for ( ++head; C() != '\0' && !isNewLine(); ++head)
-        ;
-  }
-  D(head,C(), (long)head, (int)C());
-  return C();
-}
-
-
-static H nextAtom() {
-  D(C(), head, isToken(), isIgnored(), isAtom(), isNewLine());
-  const char * const begin = head;
-  for (;C();++head)   
-    if (!isAtom())
-      break;
-  D(begin, C(), (int)C(),head);
-  char c = C();
-  C() = '\0';
-  H $ = Strings::allocate(begin);
-  C() = c;
-  D($);
-  return $;
-}
-
-static bool isAtom() {
-  return !isToken() && !isIgnored();
-}
-
-static bool isNewLine() {
-  return C() == '\n' || C() == '\r' || C() == '\0';
-}
-
-static bool isIgnored() {
-  return C() == ' ' || C() == '\t' || isNewLine() || C() < ' ' || C() >= 127;
-}
-
-static bool isToken() {
-  for (const char *p = tokens; *p; ++p)
-    if (C() == *p)
-      return true;
-  return false;
-}
+extern void reset();
+extern S result();
 }
