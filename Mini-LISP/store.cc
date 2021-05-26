e1a0e7aac6a771ac1097f810df8e8569027afbff
Yossi Gil <yogi@cs.technion.ac.il>
2021-05-27 01:23:57 +0300

    fix wrong commit #355

--- Mini-LISP/store.cc
+++ Mini-LISP/store.cc
@@ -1,3 +1,7 @@
+// Poor man's edition of a bit cleaner C/C++
+// 
+#include <cstdint> // Standard header providing integer types with widths 
+
 #define Constant(type) enum:type 
 #define Context namespace
 #define Provides extern
@@ -9,25 +13,25 @@
 #define procedure void  
 #define array(type) type *const
 
-#define text const char* const 
+#define text const char* const // Underlining representation of atoms as pointers to characters
+
 #define constant(type) type & 
 #define variable(type) const type 
 #define action void
 #define modifier 
 #define data 
 
-#include <cstdint> // Standard header providing width integer types 
+#define max(x,y) (x > y ? x : y) 
+#define min(x,y) (x < y ? x : y) 
+
 
 typedef int8_t   byte; /// Byte                         | 8 bits signed integer  | (rarely used)
 typedef int16_t  H;    /// Half a word (2 bytes)        | 16 bits signed integer | (used as handle) 
 typedef int32_t  W;    /// A word of 32 bits (2 halves) | 32 bits signed integer | (used as pair) 
 
-
 struct S { // handle of S expression
   H h; // the handle
 };
-
-typedef const char *const atom; // Underlining representation of strings as pointers to characters
 
 union pair { // The perspectives of a pair.
   W cons: 32;                         /// I.   | A single word that can 
@@ -41,18 +45,17 @@ Context Store { // Prototype for store.h
   Provides data array(pair) P;
 }
 
-Context Pairs { Provides property variable(pair) fetch(H index); }  ///  Retrieves pair of a handle 
-Context Atoms { Provides property constant(text) fetch(H index); }  ///  Retrieves text of a handle 
+Context Pairs { Provides property variable(pair) fetch(H index); }  ///  Retrieves the pair behind a positive handle (mutable)  
+Context Atoms { Provides property constant(text) fetch(H index); }  ///  Retrieves text of a handle (immutable) 
 
 Context Store { 
-  Provides modifier H yield(H, H);    /// Fetch (possibly newly allocated) pair with given contents 
-  Provides function H yield(text);    /// Fetch (possibly newly allocated) atom with given contents 
+  Provides modifier H yield(H, H);    /// Handle create new pair with given contents 
+  Provides function H yield(text);    /// Find or or create new atom with given contents 
   Provides procedure free(H index);   /// Marks a previously yielded handle as no longer in use 
 }
 
 Context Pairs { Provides function H available(); }  /// How many pairs remain available for allocation 
 Context Atoms { Provides function H available(); }  /// How many chars remain available for allocation 
-
 
 Context Store { 
   static union  {
@@ -71,88 +74,98 @@ Context Store {
   Define array(pair) P0 = all.P - 1;
   Define array(pair) P1 = all.P + all.p;
   Define array(char) A  = all.A;
-  constexpr array(char) A0  = all.A0;
-  constexpr array(char) A1  = all.A + all.a;
+  Define array(char) A0  = all.A0;
+  Define array(char) A1  = all.A + all.a;
 }
 
-#define MIN(x,y) (x < y ? x : y) 
-#define MAX(x,y) (x > y ? x : y) 
+Context Atoms { Constant(H) { $h_0$ = Store::A0 - Store::A,   $h_1$ = Store::A0 - Store::A };  } 
+Context Pairs { Constant(H) { $h_0$ = Store::P0 - Store::P,   $h_1$ = Store::P1 - Store::P };  } 
 
-
-Context Atoms { Constant(H) { min = A0 - A,   max = A0 - A };  } 
-Context Pairs { Constant(H) { min = P0 - P,   max = P1 - P };  } 
-Context Store { Constant(H) { min = MIN(Atoms::min, Pairs::min) };     
-Context Store { Constant(H) { max = MAX(Atoms::min, Pairs::min) };     
-
-Context Pairs  { Constant(H) { range = max - min + 1}; };
-Context Atoms  { Constant(H) { range = max - min + 1}; }; 
-Context Store { Constant(H) { range = max - min + 1}; };
+Context Store {   
+  Constant(H) { 
+    $h_0$ = min(Atoms::$h_0$, Pairs::$h_0$),
+    $h_1$ = max(Atoms::$h_1$, Pairs::$h_1$)
+  };     
+}
+Context Pairs  { Constant(H) { $n$ = $h_1$ - $h_1$ + 1}; };
+Context Atoms  { Constant(H) { $n$ = $h_1$ - $h_1$ + 1}; }; 
+Context Store { Constant(H)  { $n$ = $h_1$ - $h_1$ + 1}; };
 
 Context Store {
   H mark(H h)   { return h + (1 << 15); } 
-  H marked(H h) { return h < Atoms::min || h > Atoms::max; } 
+  H marked(H h) { return h < Atoms::$h_0$ || h > Atoms::$h_1$; } 
 }
 
+#undef min
+#undef max
+#undef data
+#undef function
 #include "gtest/gtest.h"
 
+int main(int argc, char **argv) {
+//  ::testing::GTEST_FLAG(filter) = "Parser*ListTokenization";//":-:*Counter*";
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(Store, minimalSize) {
+  using Context Store;
+  EXPECT_GT((H) all.a, 100);
+  EXPECT_GT((H) all.p, 100);
+};
+
+TEST(Store, overflow) {
+  using Context Store;
+  EXPECT_GT((H) all.p + 1, 0);
+  EXPECT_LT((H) - all.a - 1, 0);
+};
+
 TEST(Store, RangesAtoms) { 
-  using Context Store::Atoms; 
-  EXPECT_EQ(A + min, P + 1);
-  EXPECT_EQ(A + max, A);
-  EXPECT_EQ(range, a  - 4);
+  using Context Atoms; 
+  EXPECT_EQ(Store::A + $h_0$, Store::A0);
+  EXPECT_EQ(Store::A + $h_1$, (text) Store::P);
+  EXPECT_EQ($n$, Store::all.a - 4);
 };
 
 TEST(Store, RangesPairs) { 
-  using Context Store::Pairs; 
-  EXPECT_EQ(P + min, Store::P);
-  EXPECT_EQ(P + max, Store::P1);
-  EXPECT_EQ(range, p);
+  using Context Pairs; 
+  EXPECT_EQ(Store::A + Store::$h_0$, Store::A0);
+  EXPECT_EQ(Store::A + Store::$h_1$, Store::A);
 };
-
-
-TEST(Store, RangesPairs) { 
-  using Context Store::Pairs; 
-  EXPECT_EQ(A + Store::min, A0);
-  EXPECT_EQ(A + Store::max, A);
-};
-
-
 
 TEST(Store, MarkingPairs) { 
-  using Context Store::Pairs;
-  EXPECT_LT(Store::mark(min), Store::Atoms::min);   
-  EXPECT_LT(Store::mark(max), Store::Atoms::min);   
-  EXPECT_LT(Store::mark((min + max)/2), Store::Atoms::min);   
-  EXPECT_LT(Store::mark(max-1), Store::Atoms::min);   
-  EXPECT_LT(Store::mark(min+1), Store::Atoms::min);   
-  EXPECT_EQ(Store::mark(Store::mark(min)), min);
-  EXPECT_EQ(Store::mark(Store::mark(max)), max);
-  EXPECT_EQ(Store::mark(Store::mark((min + max)/2)),(min + max)/2);
-  EXPECT_EQ(Store::mark(Store::mark(min + 1)),min + 1 );
-  EXPECT_EQ(Store::mark(Store::mark(max + 1)), max + 1);
+  using Context Pairs;
+  EXPECT_LT(Store::mark($h_0$), Atoms::$h_0$);   
+  EXPECT_LT(Store::mark($h_1$), Atoms::$h_0$);   
+  EXPECT_LT(Store::mark(($h_0$ + $h_1$)/2), Atoms::$h_0$);   
+  EXPECT_LT(Store::mark($h_1$-1), Atoms::$h_0$);   
+  EXPECT_LT(Store::mark($h_0$+1), Atoms::$h_0$);   
+  EXPECT_EQ(Store::mark(Store::mark($h_0$)), $h_0$);
+  EXPECT_EQ(Store::mark(Store::mark($h_1$)), $h_1$);
+  EXPECT_EQ(Store::mark(Store::mark(($h_0$ + $h_1$)/2)),($h_0$ + $h_1$)/2);
+  EXPECT_EQ(Store::mark(Store::mark($h_0$ + 1)),$h_0$ + 1 );
+  EXPECT_EQ(Store::mark(Store::mark($h_1$ + 1)), $h_1$ + 1);
 };
 
 TEST(Store, MarkingAtoms) { 
-  using Context Store::Atoms;
-  EXPECT_GT(Store::mark(min), Store::Pairs::max);   
-  EXPECT_LT(Store::mark(max),0);
-  EXPECT_GT(Store::mark((min + max)/2), Store::Pairs::max);   
-  EXPECT_GT(Store::mark(max-1), Store::Pairs::max);   
-  EXPECT_GT(Store::mark(min+1), Store::Pairs::max);   
-  EXPECT_EQ(Store::mark(Store::mark(min)), min);
-  EXPECT_EQ(Store::mark(Store::mark(max)), max);
-  EXPECT_EQ(Store::mark(Store::mark((min + max)/2)),(min + max)/2);
-  EXPECT_EQ(Store::mark(Store::mark(min + 1)),min + 1 );
-  EXPECT_EQ(Store::mark(Store::mark(max + 1)), max + 1);
+  using Context Atoms;
+  EXPECT_GT(Store::mark($h_0$), Pairs::$h_1$);   
+  EXPECT_LT(Store::mark($h_1$),0);
+  EXPECT_GT(Store::mark(($h_0$ + $h_1$)/2), Pairs::$h_1$);   
+  EXPECT_GT(Store::mark($h_1$-1), Pairs::$h_1$);   
+  EXPECT_GT(Store::mark($h_0$+1), Pairs::$h_1$);   
+  EXPECT_EQ(Store::mark(Store::mark($h_0$)), $h_0$);
+  EXPECT_EQ(Store::mark(Store::mark($h_1$)), $h_1$);
+  EXPECT_EQ(Store::mark(Store::mark(($h_0$ + $h_1$)/2)),($h_0$ + $h_1$)/2);
+  EXPECT_EQ(Store::mark(Store::mark($h_0$ + 1)),$h_0$ + 1 );
+  EXPECT_EQ(Store::mark(Store::mark($h_1$ + 1)), $h_1$ + 1);
 };
 
 TEST(Store, MarkingIsMarked) { 
   using Context Store;
-  for (H h = min; h <= max; ++h)
+  for (H h = $h_0$; h <= $h_1$; ++h)
     EXPECT_TRUE(marked(mark(h)));
 };
-
-
 
 
 TEST(Store, PrimitiveSizs) { 
@@ -186,38 +199,17 @@ TEST(Store, twoArrayAreConsecutive) {
   EXPECT_LE(all.A + sizeof(all.A), (char *)all.P);
   EXPECT_EQ(all.A + sizeof(all.A) - (char *)all.P,0);
   EXPECT_EQ((void *)(all.A + sizeof all.A), (void *)all.P); 
-};
+}
 
 TEST(Store, totalSize) {
   using Context Store;
   EXPECT_EQ(sizeof all.block, all.n);
   EXPECT_EQ(sizeof all.block - all.n, 0);
   EXPECT_EQ(all.n - sizeof all.block, 0);
-};
-
+}
 TEST(Store, ConversionToIntDoesNotBreakArrayLimits) {
   using Context Store;
   EXPECT_EQ((H) all.a, all.a); 
   EXPECT_EQ((H) all.p, all.p); 
 }
 
-
-TEST(Store, minimalSize) {
-  using Context Store;
-  EXPECT_GT((H) all.a, 100);
-  EXPECT_GT((H) all.p, 100);
-};
-
-
-TEST(Store, overflow) {
-  using Context Store;
-  EXPECT_GT((H) all.p + 1, 0);
-  EXPECT_LT((H) - all.a - 1, 0);
-};
-
-
-int main(int argc, char **argv) {
-//  ::testing::GTEST_FLAG(filter) = "Parser*ListTokenization";//":-:*Counter*";
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
