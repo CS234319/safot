05d0f928293954d28bd15175e76daab04a8bfcfc
Dor Yeheskel <poogazoz123@gmail.com>
2021-05-28 15:36:13 +0300

    [issue-371] - Test cond evaluation order

--- Mini-LISP/test.h
+++ Mini-LISP/test.h
@@ -2,3 +2,16 @@
 inline auto operator == (const S s1,const S s2) { return s1.index == s2.index; }
 inline auto operator != (const S s1,const S s2) { return s1.index != s2.index; }
 inline auto operator !(const S s) { return s.null(); }
+
+inline S parse(const char *s) {
+    Parser::reset();
+    Parser::supply(strdup(s));
+    if (Parser::status() != Parser::Status::accept) {
+        throw std::runtime_error("Unexpected parse_sr error"); // should not be here - we assume all unittest inputs are valid
+    }
+    return Parser::result();
+}
+
+inline S parse(const std::string& s) {
+    return parse(s.c_str());
+}
