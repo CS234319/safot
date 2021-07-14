a747a6da8c20a51bac3f05caa6b37e81f7155c28
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-14 14:42:59 +0300

    shorter signature of extern functions

--- Mini-LISP/out.h
+++ Mini-LISP/out.h
@@ -2,9 +2,9 @@
 #define OUT_H
 #include "S.h" 
 /** Print an S expession */ extern int print(S);  
-/** Print the prompt */ extern int prompt(String  s);
+/** Print the prompt */ extern int prompt(String);
 /** Print a string error message */ extern int err(String);
 /** Print an error message associated with a handle */  extern int err(H);        
 /** Call before printing error messages */ extern int err();
-/** Call before printing to standard output */ extern int out();
+/** Call before printing normal output */ extern int out();
 #endif
