63c0da9699854418d4c7904d42f1da7c2ebcc29d
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-16 09:19:05 +0300

    Reformatting

--- Mini-LISP/Revised/layout.h
+++ Mini-LISP/Revised/layout.h
@@ -4,8 +4,8 @@
 #include "Word.h"
 Provides array(char) A;
 Provides array(Word) P;
-Let Short $M_a$ = 1 << 12; 
-Let Short $M_p$ = (1 << 15) - $M_a$ + 2; 
+Let Short $M_a$ = 1<<12; 
+Let Short $M_p$ = (1<<15) - $M_a$ + 2; 
 Let Short LIMBO = sizeof "NIL";
 Let Short $A_f$ = LIMBO - $M_a$, $A_t$ = 0, $A_n$ = range($A_f$, $A_t$), $A_x$ = $A_f$ -1;
 Let Short $P_f$ = 1, $P_t$ = $M_p$, $P_n$ = $M_p$, $P_x$ = 0;
