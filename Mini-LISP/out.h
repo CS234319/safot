81ec7e24277974d4ca41e8584b2a2b295ebdcaa7
Yossi Gil <yogi@cs.technion.ac.il>
2021-07-18 06:37:02 +0300

    Spelling errors

--- Mini-LISP/out.h
+++ Mini-LISP/out.h
@@ -1,8 +1,8 @@
 #ifndef OUT_H
 #define OUT_H
 #include "S.h" 
-/** Print an S expession */ extern int print(S);
-/** Print an S expession with newline */ extern int println(S s);
+/** Print an S expression */ extern int print(S);
+/** Print an S expression with newline */ extern int println(S s);
 /** Print the prompt */ extern int prompt(String);
 /** Print a string error message */ extern int err(String);
 /** Print an error message associated with a handle */  extern int err(H);        
