2a011d7b7562a94b7483131b4751fd14936afc06
Ofir Markowitz <ofirmarkowitz@Ofirs-MBP.018.co.il>
2021-07-26 13:14:36 +0300

    Finished parser, issues #389, #391

---
+++ Mini-Lisp.JS/Parser.test.js
@@ -0,0 +1,22 @@
+const P = require('./Parser')
+const ListCreator = require('./ListCreator')
+const Atom = require('./Atom')
+const Pair = require('./Pair')
+
+const lc = new ListCreator()
+const a = new Atom('A')
+const b = new Atom('B')
+const c = new Atom('C')
+const set = new Atom('SET')
+const quote = new Atom('QUOTE')
+
+test('parse', () => {
+
+	expect(P.parse('a')).toStrictEqual(a)
+	expect(P.parse('(a . b)')).toStrictEqual(new Pair(a, b))
+	expect(P.parse('(a b c)')).toStrictEqual(lc.create(a, b ,c))
+	expect(P.parse("(set 'b '(a . b))"))
+		.toStrictEqual(
+			lc.create(set, lc.create(quote, b), lc.create(quote, new Pair(a, b)))
+		)
+})
