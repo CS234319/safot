d0a50d2d553985caf16e2beee4cf9993206e78bc
DorYeheskel <poogazoz123@gmail.com>
2021-08-30 17:36:39 +0300

    remove hard-coded file names from the flow tests

--- Mini-Lisp.ShellTestFramework/test/flow/test_book_evaluate.py
+++ Mini-Lisp.ShellTestFramework/test/flow/test_book_evaluate.py
@@ -1,4 +1,5 @@
 import pytest
+import glob
 from pathlib import Path
 from framework.lib.flow_test_framework import FlowTestFramework
 from framework.lib.utils import get_flow
@@ -31,173 +32,15 @@ def golden_dir():
     return Path(directory)
 
 
-def test_exists(flow, input_dir, golden_dir):
+def test_book_evaluate(flow, input_dir, golden_dir):
     """
-    Mini-lisp test for: exists
-
-    Using the exists.lisp file,
-    auto generated from the Mini-lisp book
+    All the tests for all the Lisp evaluate functions,
+    which auto generated from the Mini-lisp book.
     """
-    in_file = input_dir / "test_exists.lisp.in"
-    golden_file = golden_dir / "test_exists.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_is_atomic(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: is_atomic
-
-    Using the is-atomic.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_is_atomic.lisp.in"
-    golden_file = golden_dir / "test_is_atomic.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_lookup(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: lookup
-
-    Using the lookup.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_lookup.lisp.in"
-    golden_file = golden_dir / "test_lookup.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_bind(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: bind
-
-    Using the bind.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_bind.lisp.in"
-    golden_file = golden_dir / "test_bind.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_apply_trivial_atomic(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: apply_trivial_atomic
-
-    Using the apply-trivial-atomic.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_apply_trivial_atomic.lisp.in"
-    golden_file = golden_dir / "test_apply_trivial_atomic.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_apply_eager_atomic(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: apply_eager_atomic
-
-    Using the apply-eager-atomic.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_apply_eager_atomic.lisp.in"
-    golden_file = golden_dir / "test_apply_eager_atomic.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_evaluate(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: evaluate
-    * evaluate atoms, atomic functions, cond, and errors.
-    * evaluate simple lambda/nlambda functions.
-    * evaluate complex lambda/nlambda functions.
-
-    Using the evaluate.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_evaluate.lisp.in"
-    golden_file = golden_dir / "test_evaluate.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_evaluate_list(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: evaluate_list
-
-    Using the evaluate-list.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_evaluate_list.lisp.in"
-    golden_file = golden_dir / "test_evaluate_list.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_apply_atomic(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: apply_atomic
-
-    Using the apply-atomic.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_apply_atomic.lisp.in"
-    golden_file = golden_dir / "test_apply_atomic.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_evaluate_atomic(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: evaluate_atomic
-
-    Using the evaluate-atomic.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_evaluate_atomic.lisp.in"
-    golden_file = golden_dir / "test_evaluate_atomic.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_evaluate_cond(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: evaluate_cond
-
-    Using the evaluate-cond.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_evaluate_cond.lisp.in"
-    golden_file = golden_dir / "test_evaluate_cond.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_apply_decomposed_lambda(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: apply_decomposed_lambda
-
-    Using the apply-decomposed-lambda.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_apply_decomposed_lambda.lisp.in"
-    golden_file = golden_dir / "test_apply_decomposed_lambda.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
-
-
-def test_apply(flow, input_dir, golden_dir):
-    """
-    Mini-lisp test for: apply
-
-    Using the apply.lisp file,
-    auto generated from the Mini-lisp book
-    """
-    in_file = input_dir / "test_apply.lisp.in"
-    golden_file = golden_dir / "test_apply.lisp.out"
-    out_file = flow.run_s_expr_file(in_file)
-    assert Path(out_file).read_text() == Path(golden_file).read_text()
+    input_files = sorted(glob.glob(f"{input_dir}/*.lisp.in"))
+    golden_files = sorted(glob.glob(f"{golden_dir}/*.lisp.out"))
+    for in_file, golden_file in zip(input_files, golden_files):
+        print(f"Running: {in_file}")
+        out_file = flow.run_s_expr_file(in_file)
+        assert Path(out_file).read_text() == Path(golden_file).read_text()
+        print(f"Passed.")
