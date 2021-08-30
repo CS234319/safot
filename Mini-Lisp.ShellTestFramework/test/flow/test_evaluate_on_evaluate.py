a23bce656446494720d7819338d03ab14081f602
DorYeheskel <poogazoz123@gmail.com>
2021-08-30 21:37:11 +0300

    Merge evaluate on evaluate flow tests

--- Mini-Lisp.ShellTestFramework/test/flow/test_evaluate_on_evaluate.py
+++ Mini-Lisp.ShellTestFramework/test/flow/test_evaluate_on_evaluate.py
@@ -1,41 +1,5 @@
 """
 Mini-Lisp-evaluate on Mini-Lisp-evaluate
-
-The following file includes all the tests for evaluate on evaluate,
-which based on the test: test_evaluate.in.lisp
-
-For example:
-    Original test:
-        * (evaluate 'a '(a.x t.t nil.nil)) ; X
-
-    New test:
-        * (evaluate '(evaluate (quote a) (quote (a.X t.t nil.nil))) '(environment)) ; X
-
-    Where environment includes all the definitions used by evaluate:
-        (t.t nil.nil apply.(lambda ...) evaluate.(lambda ...) quote.(nlambda ...) ...)
-
-Steps:
-    1. Generate the latest lisp files from the Mini-lisp book.
-    2. Open communication with the mini-lisp executable in Mini-Lisp.Chic.
-    3. Feed the shell with all the files from the book. (create our `executable alist`)
-    4. Generate environment (string) which will be used by the evaluate function,
-       using all the files from the book, similar to step 2. (create our `evaluate alist`)
-    5. Create our s-expression as a string (including `evaluate alist` and our `applied evaluate alist`)
-    6. Feed the shell with the input s-expression.
-    6. Communicate with the mini-lisp process.
-    7. Compare our output with expected.
-    8. If the strings are identical then the test passed, else failed and show diffs.
-Environment:
-    1. executable alist - our real alist from the C code, includes all functions
-                          that will be feeded from the book (step 3)
-
-    2. evaluate alist - our alist for the top evlauate function, includes all functions
-                          that will be feeded from the book (step 4).
-                          It (almost) similar to the executable alist, but will
-                          be executed only when running the s-expression.
-
-    3. applied evaluate alist - out alist for the applied evaluate function,
-                          can be very small (e.g: (t.t nil.nil))
 """
 import pytest
 from framework.lib.flow_test_framework import FlowTestFramework
@@ -49,7 +13,7 @@ def flow() -> FlowTestFramework:
     from the latest auto-generated files from the Mini-lisp book
     """
     generate_book_files()
-    return get_flow(polling=True, filter_newline=False)
+    return get_flow(filter_newline=False)
 
 
 @pytest.fixture
@@ -61,8 +25,10 @@ def env() -> str:
     return get_env()
 
 
-def test_lookup(flow, env):
+def test_evaluate_on_evaluate(flow, env):
     """
+    evaluate atom
+
     Original tests:
         * (evaluate 'a '(a.T t.t nil.nil)) ; T
         * (evaluate 'a '(a.x t.t nil.nil)) ; X
@@ -75,9 +41,9 @@ def test_lookup(flow, env):
     s_expr = f"(evaluate '(evaluate (quote a) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "X"
 
-
-def test_atomic_one_arguments(flow, env):
     """
+    atomic_one_arguments
+    
     Original tests:
         * (evaluate '(atom a) '(a.x t.t nil.nil))      ; T
         * (evaluate '(atom a) '(a.(x) t.t nil.nil))    ; NIL
@@ -125,9 +91,9 @@ def test_atomic_one_arguments(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (eval a)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "Y"
 
-
-def test_atomic_two_arguments(flow, env):
     """
+    atomic_two_arguments
+    
     Original tests:
         * (evaluate '(cons a b) '(a.x b.y t.t nil.nil))  ; (X.Y)
         * (evaluate '(cons a b) '(a.x b.x t.t nil.nil))  ; (X.X)
@@ -160,9 +126,9 @@ def test_atomic_two_arguments(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (set a b)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "Y"
 
-
-def test_cond(flow, env):
     """
+    cond
+    
     Original tests:
         * (evaluate '(cond (())) '(t.t nil.nil))                    ; NIL
         * (evaluate '(cond (t nil)) '(t.t nil.nil))                 ; NIL
@@ -210,9 +176,9 @@ def test_cond(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (cond (a b) (a b) (b a))) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "NIL"
 
-
-def test_id(flow, env):
     """
+    id
+    
     Original tests:
         * (evaluate '(id a) '(id.(lambda (x) x) a.t t.t nil.nil))     ; T
         * (evaluate '(id a) '(id.(lambda (x) x) a.(a b) t.t nil.nil)) ; (A B)
@@ -225,9 +191,9 @@ def test_id(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (id a)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "(A B)"
 
-
-def test_quote(flow, env):
     """
+    quote
+    
     Original tests:
         * (evaluate '(quote a) '(quote.(nlambda (x) x) a.t t.t nil.nil))  ; A
         * (evaluate '(quote bla) '(quote.(nlambda (x) x) t.t nil.nil))    ; bla
@@ -240,9 +206,9 @@ def test_quote(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (quote bla)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "BLA"
 
-
-def test_first_3(flow, env):
     """
+    first_3
+    
     Original tests:
         * (evaluate '(first a b c) '(first.(nlambda (x y z) x) t.t nil.nil)) ; a (normal)
         * (evaluate '(first a b c) '(first.(lambda (x y z) x) a.a b.b c.c t.t nil.nil)) ; a
@@ -255,9 +221,9 @@ def test_first_3(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (first a b c)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "A"
 
-
-def test_last_3(flow, env):
     """
+    last_3
+    
     Original tests:
         * (evaluate '(last a b c) '(last.(nlambda (x y z) z) t.t nil.nil)) ; c (normal)
         * (evaluate '(last a b c) '(last.(lambda (x y z) z) a.a b.b c.c t.t nil.nil)) ; c
@@ -270,9 +236,9 @@ def test_last_3(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (last a b c)) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "C"
 
-
-def test_car(flow, env):
     """
+    car
+    
     Original tests:
         * (evaluate '(xcar a) '(xcar.(lambda (x) (car x)) a.(a b) t.t nil.nil)) ; a
         * (evaluate '(xcar (a b)) '(xcar.(nlambda (x) (car x)) t.t nil.nil)) ; a (normal)
@@ -285,9 +251,9 @@ def test_car(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (xcar (a b))) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "A"
 
-
-def test_cdr(flow, env):
     """
+    cdr
+    
     Original tests:
         * (evaluate '(xcdr a) '(xcdr.(lambda (x) (cdr x)) a.(a b) t.t nil.nil)) ; b
         * (evaluate '(xcdr (a b)) '(xcdr.(nlambda (x) (cdr x)) t.t nil.nil)) ; b (normal)
@@ -300,9 +266,9 @@ def test_cdr(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (xcdr (a b))) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "(B)"
 
-
-def test_mirror(flow, env):
     """
+    mirror
+    
     Original tests:
         * (evaluate '(mirror a) '(mirror.(lambda (x) (cons (cdr x) (car x))) a.(a b) t.t nil.nil)) ; (b a)
         * (evaluate '(mirror (a b)) '(mirror.(nlambda (x) (cons (cdr x) (car x))) t.t nil.nil)) ; (b a) (normal)
@@ -316,9 +282,9 @@ def test_mirror(flow, env):
     s_expr = f"(evaluate '(evaluate (quote (mirror (a b))) (quote {alist})) '({env}))"
     assert flow.feed(s_expr) == "((B).A)"
 
-
-def test_zcar(flow, env):
     """
+    zcar
+    
     Original tests:
         * (evaluate '(zcar a) '(zcar.(lambda (x) (cond ((atom x) x) (t (car x)))) a.a t.t nil.nil))      ; a
         * (evaluate '(zcar a) '(zcar.(lambda (x) (cond ((atom x) x) (t (car x)))) a.(a b) t.t nil.nil))  ; a
