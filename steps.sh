9a785c2197794dad3aea50710333ae89b6ac6572
Yossi Gil <yogi@cs.technion.ac.il>
2021-09-21 23:44:18 +0300

    rewriting lisp.tex

--- steps.sh
+++
@@ -1,6 +0,0 @@
-% cat << EOF > Hello.java
-// Hello.java: my first Java program. It prints
-// the string "Hello, World!" on the standard output
-// stream, and then terminates.
-
-class HelloWorld {\protect \protect \unhbox \voidb@x \hbox {\LRE {\hebrewfonttt \protect \relax \protect \edef m{bx}\protect \xdef \TU/DavidCLM(0)/m/n/12 {\TU/DavidCLM(0)/m/n/12 }\TU/DavidCLM(0)/m/n/12 \size@update \enc@update \protect \unhbox \voidb@x {\protect \let \reserved@d =[\def \begingroup \def ##1,{}\toks@ {{}{},}\edef {setentrycounter[]{page}\glsnumberformat\@nil }\endgroup \let  } } } 
