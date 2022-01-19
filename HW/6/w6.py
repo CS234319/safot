17f998413bafa0e6712b81eac935300389eb1126
Yair Twito <yairt16@gmail.com>
2022-01-19 16:47:01 +0200

    fix hw6 files

--- HW/6/w6.py
+++ HW/6/w6.py
@@ -6,3 +6,7 @@ def is_a_user_defined_function(f: S_Expression) -> bool:
 def eval_defun(a_list: A_List, args: List[AST]) -> S_Expression:
     from eval import eval_rec
     raise NotImplementedError
+
+def eval_call(a_list: A_List, func: S_Expression, args: List[AST]) -> S_Expression:
+    from eval import eval_rec
+    raise NotImplementedError
