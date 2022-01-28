f5a01d7b75823e6d713c4c65cf2609e69a99ff5a
Yair Twito <yairt16@gmail.com>
2022-01-28 21:25:58 +0200

    add exams.md

--- Material/s-expression.ml
+++
@@ -1,7 +0,0 @@
-(* Recursive structure: Expressions over alphabet *)
-    datatype 'Alphabet Sx =
-             Pair of Sx * Sx 
-        |    Atom of 'Alphabet
-
-    (* Let the alphabet of atoms be all strings *)
-    type Sx = string Sx 
